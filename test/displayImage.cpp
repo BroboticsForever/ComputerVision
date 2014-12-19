1fae21a4b5bf1d526cf396f48a6e06a2dc541819
Bryan-Burkett <crazydude008@gmail.com>
2014-12-19 15:09:17 -0600

    Testing c++ code

---
+++ test/displayImage.cpp
@@ -0,0 +1,11 @@
+#include "../opencv-2.4.10/include/opencv/highgui.h"
+
+int main( int argc, char** argv ) 
+{
+	IplImage* img = cvLoadImage( argv[1] );
+	cvNamedWindow( “Example1”, CV_WINDOW_AUTOSIZE );
+	cvShowImage( “Example1”, img );
+	cvWaitKey(0);
+	cvReleaseImage( &img );
+	cvDestroyWindow( “Example1” );
+}
